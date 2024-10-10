ac84db3f0de6f99163bf4d70f3f4186550e1b449
ashok <ashoknarne@gmail.com>
2024-10-10 20:55:17 +1100

    vpc creation, subnets, route table, gateways, peering

---
+++ main.tf
@@ -0,0 +1,244 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  enable_dns_hostnames = var.enable_dns_name
+
+  tags = merge(
+            var.common_tags,
+            var.vpc_tags,
+            {
+                Name = local.resource_name
+            }
+        )
+}
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+            var.common_tags,
+            var.igw_tags,
+            {
+                Name = local.resource_name
+            }
+        )
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_cidr_blocks)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_cidr_blocks[count.index]
+  availability_zone = local.az_name[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+            var.common_tags,
+            var.public_subnet_tags,
+            {
+                Name = "${local.resource_name}-public-${local.az_name[count.index]}"
+            }
+        )
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_cidr_blocks)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_cidr_blocks[count.index]
+  availability_zone = local.az_name[count.index]
+
+  tags = merge(
+            var.common_tags,
+            var.private_subnet_tags,
+            {
+                Name = "${local.resource_name}-private-${local.az_name[count.index]}"
+            }
+        )
+}
+
+resource "aws_subnet" "database" {
+  count = length(var.database_cidr_blocks)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_cidr_blocks[count.index]
+  availability_zone = local.az_name[count.index]
+
+  tags = merge(
+            var.common_tags,
+            var.database_subnet_tags,
+            {
+                Name = "${local.resource_name}-database-${local.az_name[count.index]}"
+            }
+        )
+}
+
+# database subnet group for RDS
+resource "aws_db_subnet_group" "default" {
+  name       = local.resource_name
+  subnet_ids = aws_subnet.database[*].id
+
+  tags = merge(
+            var.common_tags,
+            var.database_subnet_group_tags,
+            {
+                Name = local.resource_name
+            }
+        )
+}
+
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+            var.common_tags,
+            var.nat_gateway_tags,
+            {
+                Name = local.resource_name
+            }
+        )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+# public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+            var.common_tags,
+            var.public_route_table_tags,
+            {
+                Name = "${local.resource_name}-public"
+            }
+        ) 
+}
+
+# private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+            var.common_tags,
+            var.private_route_table_tags,
+            {
+                Name = "${local.resource_name}-private"
+            }
+        ) 
+}
+
+# database route table
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+            var.common_tags,
+            var.database_route_table_tags,
+            {
+                Name = "${local.resource_name}-database"
+            }
+        ) 
+}
+
+# public subnets and route table associations
+resource "aws_route_table_association" "public" {
+  count = length(var.public_cidr_blocks)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# private subnets and route table associations
+resource "aws_route_table_association" "private" {
+  count = length(var.private_cidr_blocks)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# database subnets and route table associations
+resource "aws_route_table_association" "database" {
+  count = length(var.database_cidr_blocks)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
+
+# route for public route table
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = var.dest_cidr_block
+  gateway_id = aws_internet_gateway.main.id
+}
+
+# route for private route table
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = var.dest_cidr_block
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+# route for database route table
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = var.dest_cidr_block
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+# vpc peering
+resource "aws_vpc_peering_connection" "main" {
+  count = var.is_peering_required ? 1 : 0
+  vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = data.aws_vpc.default.id
+  auto_accept = true
+
+    tags = merge(
+            var.common_tags,
+            var.vpc_peering_tags,
+            {
+                Name = "${local.resource_name}-default"
+            }
+        )
+  
+}
+
+
+# route for public route table peering
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[0].id
+}
+
+# route for private route table peering
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[0].id
+}
+
+
+# route for database route table peering
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[0].id
+}
+
+# route for default route table peering
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.main.route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[0].id
+}
+
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,98 @@
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "vpc_tags" {
+    default = {}
+}
+
+variable "enable_dns_name" {
+    default = true
+}
+
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "igw_tags" {
+    default = {}
+}
+
+variable "public_cidr_blocks" {
+    type = list(string)    
+    validation {
+        condition = length(var.public_cidr_blocks) == 2
+        error_message = "Please provide two valid public subnet"
+    }
+}
+
+variable "public_subnet_tags" {
+    default = {}
+}
+
+variable "private_cidr_blocks" {
+    type = list(string)    
+    validation {
+        condition = length(var.private_cidr_blocks) == 2
+        error_message = "Please provide two valid private subnet"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
+
+variable "database_cidr_blocks" {
+    type = list(string)    
+    validation {
+        condition = length(var.database_cidr_blocks) == 2
+        error_message = "Please provide two valid database subnet"
+    }
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "database_subnet_group_tags" {
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    default = {}
+}
+
+variable "dest_cidr_block" {
+    
+}
+
+variable "is_peering_required" {
+    type = bool
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+}
+
+
+
