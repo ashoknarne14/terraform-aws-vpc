aaad5d264ccd2ce03e55dfe4347f838d95babc8d
ashok <ashoknarne@gmail.com>
2024-10-13 21:01:36 +1100

    added new outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "vpc_id" {
 output "az_info" {
     value = data.aws_availability_zones.available
 }
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
